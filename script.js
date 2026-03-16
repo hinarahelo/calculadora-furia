032ecc51a7c7bacb3e4410afe72d389c4c30d8bc
The Mônia <hinarahelo@gmail.com>
2026-03-16 13:12:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
+function dinheiro(valor){
+
+return valor.toLocaleString('pt-BR',{
+
+style:'currency',
+currency:'BRL'
+
+})
+
+}
+
+
+
 function calcularAttach(){
 
-let c4 = document.getElementById("c4").value
-let lanterna = document.getElementById("lanterna").value
-let pente = document.getElementById("pente").value
-let empunhadura = document.getElementById("empunhadura").value
-let silenciador = document.getElementById("silenciador").value
-let mira = document.getElementById("mira").value
+let c4 = Number(document.getElementById("c4").value)
+let lanterna = Number(document.getElementById("lanterna").value)
+let pente = Number(document.getElementById("pente").value)
+let emp = Number(document.getElementById("empunhadura").value)
+let sil = Number(document.getElementById("silenciador").value)
+let mira = Number(document.getElementById("mira").value)
+
+let itens = c4+lanterna+pente+emp+sil+mira
 
 let parceria =
 (c4*1500)+
 (lanterna*300)+
 (pente*300)+
-(empunhadura*300)+
-(silenciador*300)+
+(emp*300)+
+(sil*300)+
 (mira*300)
 
 let normal =
 (c4*2500)+
 (lanterna*400)+
 (pente*400)+
-(empunhadura*400)+
-(silenciador*400)+
+(emp*400)+
+(sil*400)+
 (mira*400)
 
-document.getElementById("valorParceria").innerText = parceria
-document.getElementById("valorNormal").innerText = normal
+document.getElementById("itensAttach").innerText = itens
+document.getElementById("valorParceria").innerText = dinheiro(parceria)
+document.getElementById("valorNormal").innerText = dinheiro(normal)
 
 }
 
@@ -32,28 +48,31 @@ document.getElementById("valorNormal").innerText = normal
 
 function calcularLavagem(){
 
-let bracelete = document.getElementById("bracelete").value
-let xbox = document.getElementById("xbox").value
-let notebook = document.getElementById("notebook").value
-let mouse = document.getElementById("mouse").value
-let vibrador = document.getElementById("vibrador").value
-let relogio = document.getElementById("relogio").value
-let anel = document.getElementById("anel").value
+let brace = Number(document.getElementById("bracelete").value)
+let xbox = Number(document.getElementById("xbox").value)
+let note = Number(document.getElementById("notebook").value)
+let mouse = Number(document.getElementById("mouse").value)
+let vib = Number(document.getElementById("vibrador").value)
+let rel = Number(document.getElementById("relogio").value)
+let anel = Number(document.getElementById("anel").value)
+
+let itens = brace+xbox+note+mouse+vib+rel+anel
 
 let total =
-(bracelete*600)+
+(brace*600)+
 (xbox*600)+
-(notebook*500)+
+(note*500)+
 (mouse*200)+
-(vibrador*200)+
-(relogio*500)+
+(vib*200)+
+(rel*500)+
 (anel*1000)
 
 let fac = total*0.15
 let pessoa = total*0.85
 
-document.getElementById("totalLavagem").innerText = total
-document.getElementById("valorFac").innerText = fac
-document.getElementById("valorPessoa").innerText = pessoa
+document.getElementById("itensLavagem").innerText = itens
+document.getElementById("totalLavagem").innerText = dinheiro(total)
+document.getElementById("valorFac").innerText = dinheiro(fac)
+document.getElementById("valorPessoa").innerText = dinheiro(pessoa)
 
 }
